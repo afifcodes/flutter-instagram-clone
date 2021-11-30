8be9e5ae927d340d2bc746ee146767eb9f60d4dc
afifudinn <work.afifudin@gmail.com>
2021-11-30 16:41:32 +0700

    final part 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,231 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_instagram_clone/datas/sample.dart';
+import 'package:flutter_instagram_clone/models/user.dart';
+import 'package:flutter_instagram_clone/models/post.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MaterialApp(
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+    ));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MyAppState extends State<MyApp> {
+  final List<User> users = [
+    Sample.selena,
+    Sample.bruno,
+    Sample.khoa,
+    Sample.zaksheuskaya,
+    Sample.bward,
+    Sample.angle,
+  ];
+  //////////////////////////////
+  ///--------------------------
+  ///
+  /// afifudin
+  /// @withflutter
+  ///
+  ///--------------------------
+  //////////////////////////////
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Padding(
+            padding: const EdgeInsets.only(top: 6),
+            child: Image.asset(
+              'assets/images/logo.png',
+              height: 36,
+            ),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: Image.asset('assets/icons/icon-add.png')),
+            IconButton(
+                onPressed: () {},
+                icon: Image.asset('assets/icons/icon-heart.png')),
+            IconButton(
+                onPressed: () {},
+                icon: Image.asset('assets/icons/icon-messenger.png'))
+          ],
+        ),
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+            children: [
+              Container(
+                height: 115,
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  itemCount: users.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          child: Container(
+                            width: 95,
+                            decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                gradient: LinearGradient(
+                                    colors: [
+                                      Color(0xffF7A34B),
+                                      Color(0xffDE0046)
+                                    ],
+                                    stops: [
+                                      0.4,
+                                      0.7
+                                    ],
+                                    begin: Alignment.bottomLeft,
+                                    end: Alignment.topRight)),
+                            child: Container(
+                              margin: const EdgeInsets.all(3),
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: AssetImage(
+                                          'assets/images/${users[index].profilePic}'),
+                                      fit: BoxFit.cover),
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                      color: Colors.white, width: 3)),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 6),
+                        Text(index == 0 ? 'Your Story' : users[index].username!,
+                            style: GoogleFonts.inter())
+                      ],
+                    );
+                  },
+                ),
+              ),
+              makePost(Sample.post1),
+              makePost(Sample.post2),
+            ],
+          ),
+        ));
+  }
+}
+
+Widget makePost(Post post) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Divider(
+        color: Colors.grey.shade300,
+        thickness: 1,
+        height: 1,
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      Container(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 36,
+              width: 36,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      fit: BoxFit.cover,
+                      image: AssetImage(
+                          'assets/images/${post.user!.profilePic}'))),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(
+              width: 12,
             ),
+            Text(Sample.selena.username!,
+                style: GoogleFonts.inter(
+                    fontWeight: FontWeight.w700, fontSize: 16)),
+            const Spacer(),
+            const Icon(Icons.more_horiz)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+      Image.asset('assets/images/posts/${post.image}'),
+      Container(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(children: [
+                Image.asset(
+                  'assets/icons/icon-heart.png',
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
+                Image.asset('assets/icons/icon-comment.png'),
+                const SizedBox(
+                  width: 12,
+                ),
+                Image.asset('assets/icons/icon-share.png'),
+                const Spacer(),
+                Image.asset('assets/icons/icon-bookmark.png'),
+              ]),
+              const SizedBox(
+                height: 12,
+              ),
+              Text('100 Likes',
+                  style: GoogleFonts.inter(fontWeight: FontWeight.w700)),
+              const SizedBox(
+                height: 4,
+              ),
+              RichText(
+                  text: TextSpan(
+                      style: GoogleFonts.inter(
+                          color: Colors.black, fontWeight: FontWeight.w700),
+                      text: Sample.selena.username,
+                      children: [
+                    TextSpan(
+                        style: GoogleFonts.inter(fontWeight: FontWeight.w400),
+                        text: '  ${post.caption}')
+                  ])),
+              const SizedBox(
+                height: 12,
+              ),
+              Row(
+                children: [
+                  Container(
+                      height: 30,
+                      width: 30,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/images/${Sample.selena.profilePic}'),
+                              fit: BoxFit.cover))),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Text('Add Comment...',
+                      style: GoogleFonts.inter(color: Colors.grey)),
+                  const Spacer(),
+                  const Text('😍'),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  const Text('😂'),
+                  const SizedBox(
+                    width: 24,
+                  ),
+                  Image.asset('assets/icons/add_icon.png')
+                ],
+              ),
+            ],
+          )),
+    ],
+  );
 }
