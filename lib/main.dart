74aee4b5d31b9b3827d1fd829e85a03140f8d9b6
afifudinn <work.afifudin@gmail.com>
2021-12-01 16:01:50 +0700

    final day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/datas/sample.dart';
 import 'package:flutter_instagram_clone/models/user.dart';
-import 'package:flutter_instagram_clone/models/post.dart';
+import 'package:flutter_instagram_clone/widgets/bottom_nav.dart';
+import 'package:flutter_instagram_clone/widgets/make_post.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(const MaterialApp(
@@ -26,38 +27,16 @@ class _MyAppState extends State<MyApp> {
     Sample.angle,
   ];
   //////////////////////////////
-  ///--------------------------
   ///
-  /// afifudin
-  /// @withflutter
+  /// follow @withflutter
   ///
-  ///--------------------------
   //////////////////////////////
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          title: Padding(
-            padding: const EdgeInsets.only(top: 6),
-            child: Image.asset(
-              'assets/images/logo.png',
-              height: 36,
-            ),
-          ),
-          actions: [
-            IconButton(
-                onPressed: () {},
-                icon: Image.asset('assets/icons/icon-add.png')),
-            IconButton(
-                onPressed: () {},
-                icon: Image.asset('assets/icons/icon-heart.png')),
-            IconButton(
-                onPressed: () {},
-                icon: Image.asset('assets/icons/icon-messenger.png'))
-          ],
-        ),
+        backgroundColor: Colors.white,
+        appBar: appBar(),
+        bottomNavigationBar: BottomNav(user: Sample.selena),
         body: SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
           child: Column(
@@ -111,121 +90,46 @@ class _MyAppState extends State<MyApp> {
                   },
                 ),
               ),
-              makePost(Sample.post1),
-              makePost(Sample.post2),
+              Divider(
+                color: Colors.grey.shade300,
+                thickness: 1,
+                height: 1,
+              ),
+              MakePost(post: Sample.post1),
+              MakePost(post: Sample.post2),
+              MakePost(post: Sample.post3),
+              MakePost(post: Sample.post4),
+              MakePost(post: Sample.post5),
+              MakePost(post: Sample.post6),
+              MakePost(post: Sample.post7),
+              MakePost(post: Sample.post8),
+              MakePost(post: Sample.post9),
+              MakePost(post: Sample.post10),
             ],
           ),
         ));
   }
 }
 
-Widget makePost(Post post) {
-  return Column(
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: [
-      Divider(
-        color: Colors.grey.shade300,
-        thickness: 1,
-        height: 1,
+PreferredSizeWidget appBar() {
+  return AppBar(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    title: Padding(
+      padding: const EdgeInsets.only(top: 6),
+      child: Image.asset(
+        'assets/images/logo.png',
+        height: 36,
       ),
-      Container(
-        padding: const EdgeInsets.all(12),
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              height: 36,
-              width: 36,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: AssetImage(
-                          'assets/images/${post.user!.profilePic}'))),
-            ),
-            const SizedBox(
-              width: 12,
-            ),
-            Text(Sample.selena.username!,
-                style: GoogleFonts.inter(
-                    fontWeight: FontWeight.w700, fontSize: 16)),
-            const Spacer(),
-            const Icon(Icons.more_horiz)
-          ],
-        ),
-      ),
-      Image.asset('assets/images/posts/${post.image}'),
-      Container(
-          padding: const EdgeInsets.all(12),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(children: [
-                Image.asset(
-                  'assets/icons/icon-heart.png',
-                ),
-                const SizedBox(
-                  width: 12,
-                ),
-                Image.asset('assets/icons/icon-comment.png'),
-                const SizedBox(
-                  width: 12,
-                ),
-                Image.asset('assets/icons/icon-share.png'),
-                const Spacer(),
-                Image.asset('assets/icons/icon-bookmark.png'),
-              ]),
-              const SizedBox(
-                height: 12,
-              ),
-              Text('100 Likes',
-                  style: GoogleFonts.inter(fontWeight: FontWeight.w700)),
-              const SizedBox(
-                height: 4,
-              ),
-              RichText(
-                  text: TextSpan(
-                      style: GoogleFonts.inter(
-                          color: Colors.black, fontWeight: FontWeight.w700),
-                      text: Sample.selena.username,
-                      children: [
-                    TextSpan(
-                        style: GoogleFonts.inter(fontWeight: FontWeight.w400),
-                        text: '  ${post.caption}')
-                  ])),
-              const SizedBox(
-                height: 12,
-              ),
-              Row(
-                children: [
-                  Container(
-                      height: 30,
-                      width: 30,
-                      decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                              image: AssetImage(
-                                  'assets/images/${Sample.selena.profilePic}'),
-                              fit: BoxFit.cover))),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  Text('Add Comment...',
-                      style: GoogleFonts.inter(color: Colors.grey)),
-                  const Spacer(),
-                  const Text('😍'),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  const Text('😂'),
-                  const SizedBox(
-                    width: 24,
-                  ),
-                  Image.asset('assets/icons/add_icon.png')
-                ],
-              ),
-            ],
-          )),
+    ),
+    actions: [
+      IconButton(
+          onPressed: () {}, icon: Image.asset('assets/icons/icon-add.png')),
+      IconButton(
+          onPressed: () {}, icon: Image.asset('assets/icons/icon-heart.png')),
+      IconButton(
+          onPressed: () {},
+          icon: Image.asset('assets/icons/icon-messenger.png'))
     ],
   );
 }
